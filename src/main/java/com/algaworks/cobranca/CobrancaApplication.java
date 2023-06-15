3a77fd3251decb11ce6d5f8147a13bc15d662da4
pedrohenriqueribei <pedrohenriqueribei@gmail.com>
2023-06-15 10:27:04 -0300

    Definindo configurações do localidade para pt-Br

--- src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -1,7 +1,12 @@
 package com.algaworks.cobranca;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class CobrancaApplication {
@@ -9,5 +14,12 @@ public class CobrancaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CobrancaApplication.class, args);
 	}
+	
+	
 
+	//definindo configurações de localidade Brasil
+	@Bean
+	public LocaleResolver localeResolver () {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
 }
